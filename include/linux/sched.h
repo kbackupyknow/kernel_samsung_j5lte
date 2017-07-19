58e1669ff112c8063bb667e66b77b92145aceb16
SoUnd001 <sound0020@gmail.com>
2017-07-19 20:00:58 +0300

    sched: Fix mismerge

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -171,7 +171,6 @@ extern unsigned long this_cpu_load(void);
 #ifdef CONFIG_RUNTIME_COMPCACHE
 extern unsigned long this_cpu_loadx(int i);
 #endif /* CONFIG_RUNTIME_COMPCACHE */
-extern void sched_update_nr_prod(int cpu, unsigned long nr, bool inc);
 
 extern void sched_update_nr_prod(int cpu, long delta, bool inc);
 
